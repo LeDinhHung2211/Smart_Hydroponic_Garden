f55a223ab8748a3977ab31c523bb364b0ad793d8
LeDinhHung2211 <95045569+LeDinhHung2211@users.noreply.github.com>
2024-06-08 13:59:32 +0700

    Update firebase.js

--- Code_Website_V4/firebase.js
+++ Code_Website_V4/firebase.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBA6yTZp555yZFXkCDMw93GQqe_7A58N_0",
+  apiKey: "0",
   authDomain: "esp-firebase-f07dc.firebaseapp.com",
-  databaseURL: "https://esp-firebase-f07dc-default-rtdb.firebaseio.com",
+  databaseURL: "https://esp-firebase-o.com",
   projectId: "esp-firebase-f07dc",
   storageBucket: "esp-firebase-f07dc.appspot.com",
-  messagingSenderId: "313931083002",
-  appId: "1:313931083002:web:de4674ef289e2a66972d43"
+  messagingSenderId: "",
+  appId: ""
 };
 firebase.initializeApp(firebaseConfig);
 
